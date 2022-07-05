2db88a4357ace1a86a501d3d47dba86e3c95db7e
dzaki <dzakifauzi.mubarok96@gmail.com>
2022-07-05 16:47:24 +0700

    Header Update

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,22 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { reduxAction } from "../redux/actions/action";
 // import Link from "next/link";
-import { CardEvenList } from '../components/cards';
-import Poster from '../assets/poster.png'
+import { CardEvenList } from "../components/cards";
+import Poster from "../assets/poster.png";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <p>ini head</p>
-
       </Head>
       <main>
-        <CardEvenList/>
-        <h1>
-          Hello World
-        </h1>
+        <CardEvenList />
+        <h1>Hello World</h1>
       </main>
     </div>
-        
-  )
+  );
 }
