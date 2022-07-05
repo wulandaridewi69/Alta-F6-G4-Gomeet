191ced6d317cb1ad0678db44820206bf489cb4a4
Irfan Halim <irfanhalimhidayat@gmail.com>
2022-07-05 21:44:27 +0700

    page login modified

--- pages/login.jsx
+++ pages/login.jsx
@@ -11,27 +11,25 @@ function Login() {
             <div className='bg-white/30 w-1/2 h-full flex flex-col items-center justify-evenly'>
                 <h1 className='font-bold lg:text-5xl md:text-4xl text-4xl text-white'>GOMEET</h1>
                 <div className='text-white text-4xl font-bold'>Login</div>
-                <div className='space-y-4'>
+                <div className='space-y-4 text-white'>
                     <Input
-                        id='email'
                         type='email'
-                        value='abc'
                         label='Email'
-                        variant='outlined'
-                        onChange={(e) => handleEmail(e)}
-                        onKeyDown={(e) => callSubmit(e)}
+                    />
+                    <Input
+                        type='password'
+                        label='Password'
                     />
                     <div className='flex justify-center'>
                         <Link href={'/'} >
                             <CustomButton
                                 id="btn-login"
                                 label="Login"
-                                value=""
                             />
                         </Link>
                     </div>
                     <h1 className='flex justify-center'>or</h1>
-                    <a href="#" className='underline flex justify-center'>Create Account</a>
+                    <a href="" className='underline flex justify-center'>Create Account</a>
                 </div>
             </div>
         </div>
